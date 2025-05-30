b663ea3d146eb5f6d650b3d49992d6aee1625292
Aryan Tandon <aryantandon2323@gmail.com>
2025-05-30 16:14:24 +0530

    Update rag_chain2.py
    chain type "stuff" --> "refine"

--- rag_chain2.py
+++ rag_chain2.py
@@ -290,7 +290,7 @@ Answer:""",
         
         return RetrievalQA.from_chain_type(
             llm=llm,
-            chain_type="stuff",
+            chain_type="refine",
             retriever=retriever,
             return_source_documents=False,
             chain_type_kwargs=chain_kwargs
