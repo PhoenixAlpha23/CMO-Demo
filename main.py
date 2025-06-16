da379e027d4d38afeb4a2e984d81c4ea2143b9fe
Aryan Tandon <aryantandon2323@gmail.com>
2025-06-16 17:58:10 +0530

    Logo resize

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def main():
     st.set_page_config(page_title="RAGhu", layout="wide")
     col1, col2, col3 = st.columns([2, 2, 1])
     with col2:
-        st.image("assets/cmrf logo.jpg", width=200)
+        st.image("assets/cmrf logo.jpg", width=250)
     st.markdown("<h1 style='text-align: center;'>🤖CMRF AI AGENT </h1>", unsafe_allow_html=True)
     
     if not GROQ_API_KEY:
